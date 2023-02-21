05d738fc67ebdbfc2bbf9ddf0e6e82645c7ad95c
DevidMonster <mrbat905@gmail.com>
2023-02-21 10:03:01 +0700

    update menu nav

--- src/data/index.js
+++ src/data/index.js
@@ -2,8 +2,7 @@ export const menu = [
     {id: 1,title: 'Home', link: '#home'},
     {id: 2,title: 'About', link: '#about'},
     {id: 3,title: 'Projects', link: '#project'},
-    {id: 4,title: 'Contact', link: '#contact'},
-    {id: 5,title: '.', link: '/project-admin'}
+    {id: 4,title: 'Contact', link: '#contact'}
 ]
 
 export const menuAdmin = [
